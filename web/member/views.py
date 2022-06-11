063ea8fcee11c8d0b670caf7cba1b91aaf5f6a5c
ik.nitk <ik-nitk@users.noreply.github.com>
2022-06-11 23:54:31 +0530

    Fix minor issues with previous commit (#12)

--- web/member/views.py
+++ web/member/views.py
@@ -5,17 +5,17 @@ blueprint = Blueprint(
              __name__
         )
 
-#@blueprint.route("/members")
-# def member_list_view():
-#     api = current_app.config.get('api')
-#     session = current_app.config.get('session')
-#     url = api.members
-#     response = session.get(url)
-#     response.raise_for_status()
-#     members=response.json()
-#     return render_template("members/list.html", members=members)
+@blueprint.route("/members")
+def member_list_view():
+    api = current_app.config.get('api')
+    session = current_app.config.get('session')
+    url = api.members
+    response = session.get(url)
+    response.raise_for_status()
+    members=response.json()
+    return render_template("members/list.html", members=members)
 
-@blueprint.route("/members",methods = ["GET","POST"])
+@blueprint.route("/members/create",methods = ["GET","POST"])
 def create_member():
     if request.method == 'GET':
         return render_template("members/create.html")
@@ -32,7 +32,6 @@ def create_member():
             isCore = False
         phone = request.form.get('phone_no')
         email = request.form.get('email')
-        print(idType)
         api = current_app.config.get('api')
         session = current_app.config.get('session')
         url = api.members
